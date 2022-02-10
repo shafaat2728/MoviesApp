609021f2df1daf876fbf73cc47d967eb760964c6
shafaat2728 <shafaat2728@gmail.com>
2022-02-10 23:46:13 +0500

    implementation of Motion Layout

--- app/src/main/java/com/digitify/moviesapp/data/dto/MoviesModel.kt
+++ app/src/main/java/com/digitify/moviesapp/data/dto/MoviesModel.kt
@@ -1,26 +1,32 @@
 package com.digitify.moviesapp.data.dto
 
 data class MoviesModel(
-    var results: List<Movies>?= emptyList()
+    var results: List<Movies>? = emptyList()
 )
 
 data class Movies(
     var id: Int,
-    var poster_path: String?="",
-    var release_date: String?="",
-    var title: String?=""
+    var poster_path: String? = "",
+    var release_date: String? = "",
+    var title: String? = ""
 )
 
 data class MovieDetail(
-    var backdrop_path: String?="",
+    var backdrop_path: String? = "",
     var id: Int,
-    var overview: String?="",
-    var popularity: Double?=0.0,
-    var poster_path: String?="",
-    var release_date: String?="",
-    var revenue: Int?=0,
-    var runtime: Int?=0,
-    var title: String?="",
-    var vote_average: Double?=0.0,
-    var vote_count: Int?=0
+    var overview: String? = "",
+    var popularity: Double? = 0.0,
+    var poster_path: String? = "",
+    var release_date: String? = "",
+    var genres: List<Genres>? = emptyList(),
+    var revenue: Int? = 0,
+    var runtime: Int? = 0,
+    var title: String? = "",
+    var vote_average: Double? = 0.0,
+    var vote_count: Int? = 0
+)
+
+data class Genres(
+    var id: Int,
+    var name: String? = ""
 )
